96806f2dbcbaa3a635b943b34ba46d7488084b8c
Dane O'Connor <Dane.OConnor@gmail.com>
2009-08-27 17:25:24 -0400

    Added placeholders so build script can inject proper information.

--- src/DbSnapshot/Properties/AssemblyInfo.cs
+++ src/DbSnapshot/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DbSnapshot")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyProduct("<product_name>")]
+[assembly: AssemblyCopyright("<copyright>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("<major_version>.<minor_version>.*")]
+[assembly: AssemblyFileVersion("<major_version>.<minor_version>.*")]
